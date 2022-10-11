5b14aa2af3cb5acce6e41a7cad6d61842de591df
GraceZGYG <92813221+GraceZGYG@users.noreply.github.com>
2022-10-11 15:46:32 +0800

    commit

---
+++ enumeration/enumeration.cpp
@@ -0,0 +1,22 @@
+//#include<iostream>
+//#include<cstdio>
+//using namespace std;
+//
+//int main() {
+//	int N;
+//	scanf_s("%d", &N);
+//	for (int a = 2; a <= N; ++a) 
+//		for (int b = 2; b <= a; ++b) 
+//			for (int c = b; c <= a; ++c) 
+//				for (int d = c; d <= a; ++d) {
+//					if (a * a * a == b * b * b + c * c * c + d * d * d)
+//						printf("Cube=%d,Triple=(%d,%d,%d)\n", a, b, c, d);
+//				}
+//	
+//	return 0;
+//}
+////完美立方，a^3=b^3+c^3+d^3;
+////输入：10
+////输出：Cube:10,Triple:(3,4,5)
+//
+
